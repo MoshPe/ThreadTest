0227201f6ebd1ad562421f208f2922d6e1cd243c
MoshPe <fghghf98@gmail.com>
2021-10-27 13:38:21 +0300

    .

--- src/SharedData.java
+++ src/SharedData.java
@@ -1,27 +1,53 @@
+import java.util.ArrayList;
+
+/**
+ * A <code>SharedData</code> Object to check if a sum of given array
+ * of integers is equal to a given <code>int</code> B  
+ * @author MoshPe
+ *
+ */
 public class SharedData 
 {
-	private int [] array;
+	private ArrayList<Integer> array;
 	private boolean [] winArray;
 	private boolean flag;
 	private final int b;
 	
-	public SharedData(int[] array, int b) {
+	/**
+	 * Init <code>SharedData</code> fields
+	 * @param array - given array of integer to init
+	 * @param b
+	 */
+	public SharedData(ArrayList<Integer> array, int b) {
 		
 		this.array = array;
 		this.b = b;
 	}
 
+	/**
+	 * 
+	 * @return An array of booleans, true is added to each number that was added
+	 * 			to the sum. 
+	 */
 	public boolean[] getWinArray() 
 	{
 		return winArray;
 	}
 
+	/**
+	 * Set <code>winArray</code> with given array
+	 * @param winArray
+	 */
 	public void setWinArray(boolean [] winArray) 
 	{
 		this.winArray = winArray;
 	}
 
-	public int[] getArray() 
+	/**
+	 * Return the list of given numbers
+	 * @return <code>ArrayList<Integer></code> of the given numbers
+	 */
+	public ArrayList<Integer> getArray() 
 	{
 		return array;
 	}
